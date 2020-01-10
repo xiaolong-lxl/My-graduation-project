3439df9325700c82c51e6bfe4d4a2b867dcae61f
king-long-2020 <king_long_2020@aliyun.com>
2020-01-10 14:13:30 +0800

    数据库文

--- weixiu.sql
+++ weixiu.sql
@@ -3,7 +3,6 @@ SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.7.18-log : Database - maintenance
 *********************************************************************
 */
-
 /*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
